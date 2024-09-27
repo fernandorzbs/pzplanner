ba2e2b187d1fbe5627fd1532f793e83d17f419cc
Fernando Rz <fernandorz.popup@gmail.com>
2024-09-27 15:49:09 -0400

    v1.0

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,8 @@ use Illuminate\Support\Facades\Route;
 //================================================
 
 Route::get('/', function () {
-    echo "Hola mundo";
+    echo "Hola mundo xr1@ma280nwhHSXtzaby3as";
 });
+
+161.35.54.190
+161.35.54.190
